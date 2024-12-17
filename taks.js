da8c234b4a13269936e2c8d4053cb5a400e70066
Roman Kováč <67224950+xRoko@users.noreply.github.com>
2024-12-17 18:02:25 +0100

    Create taks.js

---
+++ taks.js
@@ -0,0 +1,11 @@
+// 1. Definuj triedu Auto:
+// - Vytvor konštruktor, ktorý nastaví meno auta, pozíciu a rýchlosť
+// - Pridaj metódu 'move', ktorá posunie auto o náhodnú hodnotu podľa jeho rýchlosti
+// - Pridaj metódu 'getPosition', ktorá vráti aktuálnu pozíciu auta
+
+// 2. Vytvor inštancie dvoch áut s rôznymi menami a rýchlosťami
+
+// 3. Simuluj preteky v cykle:
+// - V cykle sa striedavo hýbu obidve autá (pre každý krok sa posunú o náhodný počet jednotiek, závislé od ich rýchlosti)
+// - Po každom kroku vypíš aktuálnu pozíciu oboch áut
+// - Pretekajte, kým jedno z áut neprejde cieľovú čiaru (napr. 100 jednotiek)
