0e0238554df9fab9dfe42074d9186964c0123668
Roman Kováč <67224950+xRoko@users.noreply.github.com>
2024-12-17 17:44:58 +0100

    Create game.js

---
+++ game.js
@@ -0,0 +1,43 @@
+// 1. Definuj triedu Gladiátor
+// - Vytvor konštruktor, ktorý nastaví meno, zdravie a silu útoku
+// - Pridaj metódu 'attack' na útok, ktorá spôsobí poškodenie nepriateľovi
+// - Pridaj metódu 'takeDamage' na prijatie poškodenia a aktualizáciu zdravia
+// - Pridaj metódu 'isAlive', ktorá vracia, či je gladiátor nažive
+
+class Gladiator {
+    constructor(name, health, attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+
+    // Metóda na útok
+    attack(monster) {
+    }
+
+    // Metóda na prijatie poškodenia
+    takeDamage(damage) {
+    }
+
+    // Metóda na kontrolu, či je gladiátor nažive
+    isAlive() {
+    }
+}
+
+
+// 2. Definuj triedu Príšera
+// - Vytvor konštruktor, ktorý nastaví meno, zdravie a silu útoku
+// - Pridaj metódu 'attack' na útok, ktorá spôsobí poškodenie gladiátorovi
+// - Pridaj metódu 'takeDamage' na prijatie poškodenia a aktualizáciu zdravia
+// - Pridaj metódu 'isAlive', ktorá vracia, či je príšera nažive
+
+// 3. Vytvor inštancie (objekty) pre gladiátora a príšeru s vhodnými hodnotami
+// - Gladiátor bude mať meno, zdravie a silu útoku
+// - Príšera bude mať meno, zdravie a silu útoku
+
+// 4. Implementuj bojový cyklus:
+// - Používaj cyklus (napr. while), ktorý striedavo útočí gladiátor a príšera
+// - Po každom útoku kontroluj, či je niekto mŕtvy (pomocou isAlive)
+// - Ak je niekto mŕtvy, koniec boja a vypíš víťaza
+
+// 5. Na konci boja vypíš, kto vyhral
