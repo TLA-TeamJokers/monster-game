47dde24e41bebcfa62d246e03508a9c17ae2a63b
Daniel Vala <daniel.vala@outlook.com>
2024-12-18 18:31:03 +0100

    Classes exercise v1

--- game.js
+++ game.js
@@ -13,17 +13,27 @@ class Gladiator {
 
     // Metóda na útok
     attack(monster) {
+        monster.takeDamage(this.attackPower);
     }
 
     // Metóda na prijatie poškodenia
     takeDamage(damage) {
+        if (this.health > 1) {
+            this.health -= damage;
+        }
+
+        console.log(`Gladiator: ${this.name} has ${this.health} health.`);
     }
 
     // Metóda na kontrolu, či je gladiátor nažive
     isAlive() {
+        if (this.health <= 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
-
 
 // 2. Definuj triedu Príšera
 // - Vytvor konštruktor, ktorý nastaví meno, zdravie a silu útoku
@@ -31,13 +41,57 @@ class Gladiator {
 // - Pridaj metódu 'takeDamage' na prijatie poškodenia a aktualizáciu zdravia
 // - Pridaj metódu 'isAlive', ktorá vracia, či je príšera nažive
 
+class Monster {
+    constructor(name, health, attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+
+    attack(gladiator) {
+        gladiator.takeDamage(this.attackPower);
+    }
+
+    takeDamage(damage) {
+        if (this.health > 1) {
+            this.health -= damage;
+        }
+
+        console.log(`Monster: ${this.name} has ${this.health} health.`);
+    }
+
+    isAlive() {
+        if (this.health <= 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
 // 3. Vytvor inštancie (objekty) pre gladiátora a príšeru s vhodnými hodnotami
 // - Gladiátor bude mať meno, zdravie a silu útoku
 // - Príšera bude mať meno, zdravie a silu útoku
+
+let monster1 = new Monster('Voidling', 10, 2);
+let monster2 = new Monster('Void Herald', 30, 5);
+let gladiator1 = new Gladiator('Jayce', 20, 4);
+let gladiator2 = new Gladiator('Victor', 25, 5);
 
 // 4. Implementuj bojový cyklus:
 // - Používaj cyklus (napr. while), ktorý striedavo útočí gladiátor a príšera
 // - Po každom útoku kontroluj, či je niekto mŕtvy (pomocou isAlive)
 // - Ak je niekto mŕtvy, koniec boja a vypíš víťaza
 
+while (monster1.isAlive() && gladiator1.isAlive()) {
+    gladiator1.attack(monster1);
+    monster1.attack(gladiator1);
+}
+
 // 5. Na konci boja vypíš, kto vyhral
+
+if (gladiator1.isAlive()) {
+    console.log(`${gladiator1.name} is Victorious!`)
+} else {
+    console.log(`${gladiator1.name} was Deafeated!`)
+}
--- taks.js
+++ taks.js
@@ -2,10 +2,67 @@
 // - Vytvor konštruktor, ktorý nastaví meno auta, pozíciu a rýchlosť
 // - Pridaj metódu 'move', ktorá posunie auto o náhodnú hodnotu podľa jeho rýchlosti
 // - Pridaj metódu 'getPosition', ktorá vráti aktuálnu pozíciu auta
+class Car {
+    constructor(name,position,speedStraight, speedCurves) {
+        this.name = name;
+        this.position = position;
+        this.distance = 0;
+        this.speedStraight = speedStraight;
+        this.speedCurves = speedCurves;
+    }
 
+    move(trackPart) {
+        if (trackPart === 'straight') {
+            let racingLine = Math.floor(Math.random() * (100 - 90 + 1)) + 90;
+            let traveled = parseInt(this.speedStraight * (racingLine / 100));
+            this.distance += traveled;
+        }
+        if (trackPart === 'curve') {
+            let racingLine = Math.floor(Math.random() * (100 - 80 + 1)) + 80;
+            let traveled = parseInt(this.speedCurves * (racingLine / 100));
+            this.distance += traveled;
+        }
+    }
+
+    getPosition(car1, car2) {
+        if (car1.distance > car2.distance) {
+            car1.position = 1;
+            car2.position = 2;
+        }
+        else if (car1.distance < car2.distance) {
+            car1.position = 2;
+            car2.position = 1;
+        }
+        console.log(`${car1.name} is now ${car1.position} and traveled ${car1.distance}`);
+    }
+}
 // 2. Vytvor inštancie dvoch áut s rôznymi menami a rýchlosťami
+let car1 = new Car('Ferrari', 1, 330, 270);
+let car2 = new Car('McLaren', 2, 325, 280);
 
 // 3. Simuluj preteky v cykle:
 // - V cykle sa striedavo hýbu obidve autá (pre každý krok sa posunú o náhodný počet jednotiek, závislé od ich rýchlosti)
 // - Po každom kroku vypíš aktuálnu pozíciu oboch áut
 // - Pretekajte, kým jedno z áut neprejde cieľovú čiaru (napr. 100 jednotiek)
+let monzaTrack = ['straight', 'straight', 'curve', 'curve','straight', 'curve','straight','curve','curve','straight','curve','straight','straight','curve','curve','straight'];
+let monzaLength = 10000;
+let trackPart = 0;
+
+while (car1.distance < monzaLength && car2.distance < monzaLength ) {
+    if (car1.position > car2.position) {
+        car1.move(monzaTrack[trackPart]);
+        car2.move(monzaTrack[trackPart]);
+    } else {
+        car2.move(monzaTrack[trackPart]);
+        car1.move(monzaTrack[trackPart]);
+    }
+    car1.getPosition(car1, car2);
+    car2.getPosition(car2, car1);
+    trackPart = (trackPart + 1) % monzaTrack.length;
+}
+
+if (car1.position === 1) {
+    console.log(`${car1.name} is the Winner!`);
+} else {
+    console.log(`${car2.name} is the Winner!`);
+}
