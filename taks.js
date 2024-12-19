2383dd6e1e53815d755d67d8c3659c3eb14dd846
Andrej2610 <andrejapyro@seznam.cz>
2024-12-19 08:27:37 +0100

    i will try:)

--- taks.js
+++ taks.js
@@ -9,3 +9,51 @@
 // - V cykle sa striedavo hýbu obidve autá (pre každý krok sa posunú o náhodný počet jednotiek, závislé od ich rýchlosti)
 // - Po každom kroku vypíš aktuálnu pozíciu oboch áut
 // - Pretekajte, kým jedno z áut neprejde cieľovú čiaru (napr. 100 jednotiek)
+
+
+
+class Auto {
+    constructor(jmeno, rychlost) {
+        this.jmeno = jmeno;
+        this.rychlost = rychlost;
+    }
+}
+
+let auto1 = new Auto("Saab", 25);
+let auto2 = new Auto("Ford", 30);
+
+console.log(`Auto 1: Jméno: ${auto1.jmeno}, Rychlost: ${auto1.rychlost}`);
+console.log(`Auto 2: Jméno: ${auto2.jmeno}, Rychlost: ${auto2.rychlost}`);
+
+const finish = 100;
+
+let vzdalenostAuto1 = 0;
+let vzdalenostAuto2 = 0;
+
+console.log("Jedeme!");
+
+function start() {
+    let krok = 1;
+    while (true) {
+
+        console.log(`Krok ${krok}`);
+
+        vzdalenostAuto1 += Math.random() * auto1.rychlost;
+
+        vzdalenostAuto2 += Math.random() * auto2.rychlost;
+
+        console.log(`${auto1.jmeno}  celkem ujeto: ${vzdalenostAuto1.toFixed(1)}`);
+        console.log(`${auto2.jmeno}  celkem ujeto: ${vzdalenostAuto2.toFixed(1)}`);
+
+        if (vzdalenostAuto1 >= finish) {
+            console.log(`${auto1.jmeno} Gratuluji vyhravas!`);
+            return;
+        }
+        if (vzdalenostAuto2 >= finish) {
+            console.log(`${auto2.jmeno} Gratuluji vyhravas!`);
+            return;
+        }
+        krok++;
+    }
+}
+start();
